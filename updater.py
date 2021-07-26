1955e2db2d5a811da79cad9f23cd74a93c7b783b
AwesomeKids652 <69184520+AwesomeKids652@users.noreply.github.com>
2021-07-26 16:25:49 +0530

    Create updater.py

---
+++ updater.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+import subprocess
+import os
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gitpython'])
+import git as gp
+os.remove(*)
+gp.Git("//updatel").clone("git://github.com/AwesomeKids652/pymc/pymc.git")
+from shutil import *
+shutil.copy2('//update1//*', '//')
+print("updated! restart program")
+sleep(2000000)
+sys.exit()
